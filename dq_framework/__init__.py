f7b88e5c154f843a278c0fc75f23ab415e28f410
tatyana-kutkina <kutkina_tu@gkl-kemerovo.ru>
2023-04-22 09:25:23 +0300

    Add init file

---
+++ dq_framework/__init__.py
@@ -0,0 +1,7 @@
+"""
+The dq_framework module provides metrics for data quality checking and
+tool for readable and convenient data quality reporting
+"""
+
+import dq_framework.metrics
+import dq_framework.report
