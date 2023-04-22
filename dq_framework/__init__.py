ac0fb68cb94a0b58cc3144145932ad05199e1c81
Tatyana Kutkina <44206575+tatyana-kutkina@users.noreply.github.com>
2023-04-22 12:54:46 +0300

    rename package

--- dq_framework/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-The dq_framework module provides metrics for data quality checking and
-tool for readable and convenient data quality reporting
-"""
-
-import dq_framework.metrics
-import dq_framework.report
