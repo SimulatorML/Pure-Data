4bad77414e1232d03d851356e87e52c6ba33cb5d
Alexey Averyanov <averyanovalexey@gmail.com>
2023-04-23 17:20:44 +0600

    del import pd

--- pure/tests/test_metrics.py
+++ pure/tests/test_metrics.py
@@ -1,12 +1,7 @@
-from pure import metrics
-
-
-import pandas as pd
 import numpy as np
+from test_fixtures.tables import TABLES1, TABLES2
 
-
-from test_fixtures.tables import TABLES1
-from test_fixtures.tables import TABLES2
+from pure import metrics
 
 
 def test_pandas_count_total():
@@ -17,43 +12,38 @@ def test_pandas_count_total():
     table = TABLES1["sales"].copy()
     metric_result = metrics.CountTotal()(table)
 
-    msg = (
-        f"Metric 'CountTotal' should "
-        "return Dict[str, Any[float, int, str]]"
-    )
+    msg = f"Metric 'CountTotal' should " "return Dict[str, Any[float, int, str]]"
     assert isinstance(metric_result, dict), msg
 
-    expected_result = {'total': 7}
+    expected_result = {"total": 7}
     msg = (
         f"Metric {metrics.CountTotal.__name__} returns wrong value."
         f" Yours value: {metric_result}. Valid value: {expected_result}"
     )
     assert metric_result == expected_result, msg
-
 
     # TABLE2
 
     table = TABLES2["sales"].copy()
     metric_result = metrics.CountTotal()(table)
 
-    expected_result = {'total': 21}
+    expected_result = {"total": 21}
     msg = (
         f"Metric {metrics.CountTotal.__name__} returns wrong value."
         f" Yours value: {metric_result}. Valid value: {expected_result}"
     )
     assert metric_result == expected_result, msg
 
-    
 
 def test_pandas_count_zeros():
     """Test CountZeros metric (pandas engine)."""
-    
+
     # TABLE1
 
     table = TABLES1["sales"].copy()
     metric_result = metrics.CountZeros("qty")(table)
 
-    expected_result = {'total': 7, 'count': 1, 'delta': 0.14285}
+    expected_result = {"total": 7, "count": 1, "delta": 0.14285}
     msg = (
         f"Metric {metrics.CountZeros.__name__} returns wrong value."
         f" Yours value: {metric_result}. Valid value: {expected_result}"
@@ -66,11 +56,11 @@ def test_pandas_count_zeros():
     table = TABLES2["sales"].copy()
     metric_result = metrics.CountZeros("qty")(table)
 
-    expected_result = {'total': 21, 'count': 2, 'delta': 0.09523}
+    expected_result = {"total": 21, "count": 2, "delta": 0.09523}
     msg = (
         f"Metric {metrics.CountZeros.__name__} returns wrong value."
         f" Yours value: {metric_result}. Valid value: {expected_result}"
     )
-    
+
     for key, value in metric_result.items():
         assert np.isclose(value, expected_result[key], rtol=1e-04), msg
