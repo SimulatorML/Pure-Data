dba2a98af0b9c3da09950103ca176040298852b1
Tatyanna-Kutkina <tatyanakutkina.tk@gmail.com>
2023-04-29 14:58:43 +0300

    Add docstring

--- pure/tests/test_metrics.py
+++ pure/tests/test_metrics.py
@@ -90,21 +90,37 @@ def run_one_pyspark_test(metric_name):
 
 
 def test_count_total():
+    """Test CountTotal metric.
+
+    Test that both, pandas and pyspark, metric realizations
+    give correct results on given test cases.
+    """
     run_one_pandas_test("CountTotal")
     run_one_pyspark_test("CountTotal")
 
 
 def test_count_zeros():
+    """Test CountZeros metric.
+
+    Test that both, pandas and pyspark, metric realizations
+    give correct results on given test cases.
+    """
     run_one_pandas_test("CountZeros")
     run_one_pyspark_test("CountZeros")
 
 
 def test_count_null():
+    """Test CountNull metric, both pandas and pyspark."""
     run_one_pandas_test("CountNull")
     run_one_pyspark_test("CountNull")
 
 
 def test_count_null_wrong_agg():
+    """Test CountNull metric initialization.
+
+    Test that metric initialization with unacceptable
+    aggregation parameter value raises ValueError.
+    """
     try:
         metrics.CountNull(['qty'], 'ALL')
     except ValueError:
@@ -114,56 +130,71 @@ def test_count_null_wrong_agg():
 
 
 def test_count_duplicates():
+    """Test CountDuplicates metric, both pandas and pyspark."""
     run_one_pandas_test("CountDuplicates")
     run_one_pyspark_test("CountDuplicates")
 
 
 def test_count_value():
+    """Test CountValue metric, both pandas and pyspark."""
     run_one_pandas_test("CountValue")
     run_one_pyspark_test("CountValue")
 
 
 def test_count_below_value():
+    """Test CountBelowValue metric, both pandas and pyspark."""
     run_one_pandas_test("CountBelowValue")
     run_one_pyspark_test("CountBelowValue")
 
 
 def test_count_greater_value():
+    """Test CountGreaterValue metric, both pandas and pyspark."""
     run_one_pandas_test("CountGreaterValue")
     run_one_pyspark_test("CountGreaterValue")
 
 
 def test_count_below_column():
+    """Test CountBelowColumn metric, both pandas and pyspark."""
     run_one_pandas_test("CountBelowColumn")
     run_one_pyspark_test("CountBelowColumn")
 
 
 def test_count_ration_below():
+    """Test CountRatioBelow metric, both pandas and pyspark."""
     run_one_pandas_test("CountRatioBelow")
     run_one_pyspark_test("CountRatioBelow")
 
 
 def test_count_cb():
+    """Test CountCB metric, both pandas and pyspark."""
     run_one_pandas_test("CountCB")
     run_one_pyspark_test("CountCB")
 
 
 def test_count_lag():
+    """Test CountLag metric, both pandas and pyspark."""
     run_one_pandas_test("CountLag")
     run_one_pyspark_test("CountLag")
 
 
 def test_count_value_in_required_set():
-    run_one_pandas_test("CountValueRequiredSet")
-    run_one_pyspark_test("CountValueRequiredSet")
+    """Test CountValueInRequiredSet metric, both pandas and pyspark."""
+    run_one_pandas_test("CountValueInRequiredSet")
+    run_one_pyspark_test("CountValueInRequiredSet")
 
 
 def test_count_value_in_bounds():
+    """Test CountValueInBounds metric, both pandas and pyspark."""
     run_one_pandas_test("CountValueInBounds")
     run_one_pyspark_test("CountValueInBounds")
 
 
 def test_count_value_in_bound_wrong_bounds():
+    """Test CountValueInBounds metric initialization.
+
+    Test that metric initialization with 'lower_bound' parameter
+    greater than 'upper_bound' parameter raises ValueError.
+    """
     try:
         metrics.CountValueInBounds('qty', 10, 5, False)
     except ValueError:
@@ -173,11 +204,17 @@ def test_count_value_in_bound_wrong_bounds():
 
 
 def test_count_extreme_values_formula():
+    """Test CountExtremeValuesFormula metric, both pandas and pyspark."""
     run_one_pandas_test("CountExtremeValuesFormula")
     run_one_pyspark_test("CountExtremeValuesFormula")
 
 
 def test_count_extreme_values_formula_wrong_style():
+    """Test CountExtremeValuesFormula metric initialization.
+
+    Test that metric initialization with unacceptable
+    'style' parameter raises ValueError.
+    """
     try:
         metrics.CountExtremeValuesFormula('qty', 1, 'equal')
     except ValueError:
@@ -187,11 +224,17 @@ def test_count_extreme_values_formula_wrong_style():
 
 
 def test_count_extreme_values_quantile():
+    """Test CountExtremeValuesQuantile metric, both pandas and pyspark."""
     run_one_pandas_test("CountExtremeValuesQuantile")
     run_one_pyspark_test("CountExtremeValuesQuantile")
 
 
 def test_count_extreme_values_quantile_wrong_style():
+    """Test CountExtremeValuesQuantile metric initialization.
+
+    Test that metric initialization with unacceptable
+    'style' parameter raises ValueError.
+    """
     try:
         metrics.CountExtremeValuesQuantile('qty', 0.2, 'equal')
     except ValueError:
@@ -201,6 +244,11 @@ def test_count_extreme_values_quantile_wrong_style():
 
 
 def test_count_extreme_values_quantile_wrong_q():
+    """Test CountExtremeValuesQuantile metric initialization.
+
+    Test that metric initialization with
+    'number' parameter out of acceptable [0, 1] interval raises ValueError.
+    """
     try:
         metrics.CountExtremeValuesQuantile('qty', 2, 'greater')
     except ValueError:
@@ -210,11 +258,17 @@ def test_count_extreme_values_quantile_wrong_q():
 
 
 def test_count_last_day_rows():
+    """Test CountLastDayRows metric, both pandas and pyspark."""
     run_one_pandas_test("CountLastDayRows")
     run_one_pyspark_test("CountLastDayRows")
 
 
 def test_count_last_day_rows_negative_percent():
+    """Test CountLastDayRows metric initialization.
+
+    Test that metric initialization with
+    negative 'percent' parameter value raises ValueError.
+    """
     try:
         metrics.CountLastDayRows('day', -10)
     except ValueError:
@@ -224,6 +278,15 @@ def test_count_last_day_rows_negative_percent():
 
 
 def test_count_few_last_day_rows():
+    """Test CountFewLastDayRows metric, pandas version."""
+
+
+def test_count_vew_last_day_rows_pyspark():
+    """Test CountFewLastDayRows metric, pyspark version (not implemented yet).
+
+    Test that calling pyspark version of CountFewLastDayRows
+    raises NotImplementedError .
+    """
     run_one_pandas_test("CountFewLastDayRows")
     try:
         run_one_pyspark_test("CountFewLastDayRows")
@@ -234,6 +297,11 @@ def test_count_few_last_day_rows():
 
 
 def test_count_few_last_day_rows_negative_percent():
+    """Test CountFewLastDayRows metric initialization.
+
+    Test that metric initialization with
+    negative 'percent' parameter value raises ValueError.
+    """
     try:
         metrics.CountFewLastDayRows('day', -10)
     except ValueError:
@@ -243,6 +311,11 @@ def test_count_few_last_day_rows_negative_percent():
 
 
 def test_count_few_last_day_rows_negative_number():
+    """Test CountFewLastDayRows metric initialization.
+
+    Test that metric initialization with
+    negative 'number' parameter value raises ValueError.
+    """
     try:
         metrics.CountFewLastDayRows('day', 40, -2)
     except ValueError:
@@ -252,6 +325,11 @@ def test_count_few_last_day_rows_negative_number():
 
 
 def test_count_few_last_day_rows_number_greater():
+    """Test CountFewLastDayRows metric, pandas version, with inappropriate 'number' parameter.
+
+    Test that metric calling with 'number' value greater
+    than number of unique days in dataset raises ValueError.
+    """
     try:
         model = metrics.CountFewLastDayRows('day', 40, 8)
         df = TABLES2["sales"]
@@ -260,5 +338,3 @@ def test_count_few_last_day_rows_number_greater():
         pass
     else:
         raise AssertionError("Number greater than number of days in dataset is not handled.")
-
-
