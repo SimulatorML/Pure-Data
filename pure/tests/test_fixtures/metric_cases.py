d89299c7689d25266f345d287b7ad14127067c45
Tatyanna-Kutkina <tatyanakutkina.tk@gmail.com>
2023-04-29 14:31:55 +0300

    Add new metric test and test cases

--- pure/tests/test_fixtures/metric_cases.py
+++ pure/tests/test_fixtures/metric_cases.py
@@ -1,6 +1,8 @@
 """Test cases."""
 from test_fixtures.tables import TABLES1, TABLES2
+import datetime as dt
 
+today = dt.datetime.now()
 # Each metric can have multiple test cases with different tables,
 # params and expected results.
 TEST_CASES = {
@@ -30,6 +32,58 @@ TEST_CASES = {
             "table_name": "sales",
             "params": ["qty"],
             "expected_result": {"total": 21, "count": 2, "delta": 0.09523},
+        },
+    ],
+    "CountNull": [
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": [["qty"], 'any'],
+            "expected_result": {"total": 7, "count": 2, "delta": 0.285714},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": [["qty", "price"], 'any'],
+            "expected_result": {"total": 21, "count": 6, "delta": 0.28571},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": [["qty", "price"], 'all'],
+            "expected_result": {"total": 7, "count": 1, "delta": 0.142857},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": [["qty", "price"], 'all'],
+            "expected_result": {"total": 21, "count": 0, "delta": 0.0},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": [["price"], 'all'],
+            "expected_result": {"total": 21, "count": 0, "delta": 0.0},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": [["qty"], 'all'],
+            "expected_result": {"total": 21, "count": 6, "delta": 0.28571},
+        },
+    ],
+    "CountDuplicates": [
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": [["item_id", "qty"]],
+            "expected_result": {"total": 7, "count": 1, "delta": 0.14285},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": [["item_id", "qty"]],
+            "expected_result": {"total": 21, "count": 11, "delta": 0.523809},
         },
     ],
     "CountValue": [
@@ -118,14 +172,200 @@ TEST_CASES = {
             "expected_result": {"total": 7, "count": 5, "delta": 0.71428},
         },
     ],
-    "CountRatioBelow": [],
-    "CountCB": [],
-    "CountLag": [],
-    "CountValueInRequiredSet": [],
-    "CountValueOutOfBounds": [],
-    "CountExtremeValuesFormula": [],
-    "CountExtremeValuesQuantile": [],
-    "CountLastDayRows": [],
-    "CountFewLastDayRows": [],
-
+    "CountRatioBelow": [
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ["qty", "price", "revenue", True],
+            "expected_result": {"total": 7, "count": 4, "delta": 0.57143},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ["qty", "price", "revenue", False],
+            "expected_result": {"total": 7, "count": 5, "delta": 0.71429},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["qty", "price", "revenue", True],
+            "expected_result": {"total": 21, "count": 11, "delta": 0.52381},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["qty", "price", "revenue", False],
+            "expected_result": {"total": 21, "count": 13, "delta": 0.61905},
+        },
+    ],
+    "CountCB": [
+        # TODO: add cases
+    ],
+    "CountLag": [
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["day", "%Y-%m-%d"],
+            "expected_result": {"today": today.strftime("%Y-%m-%d"), "last_day": "2022-10-31",
+                                "lag": (today - dt.datetime(2022, 10, 31)).days},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "views",
+            "params": ["dt", "%Y-%m-%d"],
+            "expected_result": {"today": today.strftime("%Y-%m-%d"), "last_day": "2022-09-25",
+                                "lag": (today - dt.datetime(2022, 9, 25)).days},
+        },
+    ],
+    "CountValueInRequiredSet": [
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ["pay_card", {"visa", "mastercard"}],
+            "expected_result": {"total": 7, "count": 4, "delta": 4 / 7},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["pay_card", {"visa", "mastercard", "мир"}],
+            "expected_result": {"total": 21, "count": 16, "delta": 16 / 21},
+        },
+    ],
+    "CountValueInBounds": [
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ['qty', 1, 8, True],
+            "expected_result": {"total": 7, "count": 2, "delta": 0.28571},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ['qty', 1, 8, False],
+            "expected_result": {"total": 7, "count": 3, "delta": 0.42857},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ['qty', 1, 8, True],
+            "expected_result": {"total": 21, "count": 10, "delta": 0.47619},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ['qty', 1, 8, False],
+            "expected_result": {"total": 21, "count": 13, "delta": 0.61905},
+        },
+    ],
+    "CountExtremeValuesFormula": [
+        {
+            "tables_set": TABLES1,
+            "table_name": "views",
+            "params": ['views', 1, 'greater'],
+            "expected_result": {"total": 9, "count": 1, "delta": 0.11111},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "views",
+            "params": ['views', 1, 'lower'],
+            "expected_result": {"total": 9, "count": 1, "delta": 0.11111},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ['qty', 1, 'greater'],
+            "expected_result": {"total": 7, "count": 1, "delta": 0.14286},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ['qty', 1, 'lower'],
+            "expected_result": {"total": 7, "count": 1, "delta": 0.14286},
+        },
+    ],
+    "CountExtremeValuesQuantile": [
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["qty", 0.8, 'greater'],
+            "expected_result": {"total": 21, "count": 2, "delta": 0.09524},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["qty", 0.2, 'lower'],
+            "expected_result": {"total": 21, "count": 2, "delta": 0.09524},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ["qty", 0.8, 'greater'],
+            "expected_result": {"total": 7, "count": 1, "delta": 0.14286},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "sales",
+            "params": ["qty", 0.2, 'lower'],
+            "expected_result": {"total": 7, "count": 0, "delta": 0.0},
+        },
+    ],
+    "CountLastDayRows": [
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["day", 80],
+            "expected_result": {'average': 2.85714, 'last_date_count': 1, 'percentage': 35.0,
+                                'at_least_80%': False},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["day", 35],
+            "expected_result": {'average': 2.85714, 'last_date_count': 1, 'percentage': 35.0,
+                                'at_least_35%': True},
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "views",
+            "params": ["dt", 80],
+            "expected_result": {'average': 3, 'last_date_count': 6, 'percentage': 200.0,
+                                'at_least_80%': True},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "two_years",
+            "params": ["dt", 80],
+            "expected_result": {'average': 1.5, 'last_date_count': 3, 'percentage': 200.0,
+                                'at_least_80%': True},
+        },
+    ],
+    "CountFewLastDayRows": [
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["day", 80, 3],
+            "expected_result": {'average': 3.2, 'days': 1}
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "sales",
+            "params": ["day", 30, 3],
+            "expected_result": {'average': 3.2, 'days': 3}
+        },
+        {
+            "tables_set": TABLES2,
+            "table_name": "views",
+            "params": ["dt", 80, 2],
+            "expected_result": {'average': 3, 'days': 2},
+        },
+        {
+            "tables_set": TABLES1,
+            "table_name": "two_years",
+            "params": ["dt", 80, 2],
+            "expected_result": {'average': 1.33333, 'days': 2},
+        },
+    ],
+    "CheckAdversarialValidation":[
+        # TODO: add cases
+    ]
 }
--- pure/tests/test_metrics.py
+++ pure/tests/test_metrics.py
@@ -1,36 +1,17 @@
 import numpy as np
 from test_fixtures.metric_cases import TEST_CASES as metric_cases
+from test_fixtures.tables import TABLES1, TABLES2
 
 from pure import metrics
 from pyspark.sql import SparkSession
+import numbers
 
 
 def test_metrics_pandas():
     """Test pandas engine metrics."""
 
-    for metric_name, test_cases in metric_cases.items():
-        for case in test_cases:
-
-            tables_set = case["tables_set"]
-            table_name = case["table_name"]
-            params = case["params"]
-            expected_result = case["expected_result"]
-
-            table = tables_set[table_name].copy()
-            metric_result = getattr(metrics, metric_name)(*params)(table)
-
-            msg = (
-                f"Metric '{metric_name}' should "
-                "return Dict[str, Any[float, int, str]]"
-            )
-            assert isinstance(metric_result, dict), msg
-
-            msg = (
-                f"Metric {metric_name} returns wrong value."
-                f" Yours value: {metric_result}. Valid value: {expected_result}"
-            )
-            for key, value in metric_result.items():
-                assert np.isclose(value, expected_result[key], rtol=1e-04), msg
+    for metric_name in metric_cases.keys():
+        run_one_pandas_test(metric_name)
 
 
 def test_metrics_pyspark():
@@ -40,30 +21,8 @@ def test_metrics_pyspark():
     )
     spark.sparkContext.setLogLevel("OFF")
 
-    for metric_name, test_cases in metric_cases.items():
-        for case in test_cases:
-
-            tables_set = case["tables_set"]
-            table_name = case["table_name"]
-            params = case["params"]
-            expected_result = case["expected_result"]
-
-            table = tables_set[table_name].copy()
-            table_spark = spark.createDataFrame(table)
-            metric_result = getattr(metrics, metric_name)(*params)(table_spark)
-
-            msg = (
-                f"Metric '{metric_name}' should "
-                "return Dict[str, Any[float, int, str]]"
-            )
-            assert isinstance(metric_result, dict), msg
-
-            msg = (
-                f"Metric {metric_name} returns wrong value."
-                f" Yours value: {metric_result}. Valid value: {expected_result}"
-            )
-            for key, value in metric_result.items():
-                assert np.isclose(value, expected_result[key], rtol=1e-04), msg
+    for metric_name in metric_cases.keys():
+        run_one_pyspark_test(metric_name)
 
 
 def run_one_pandas_test(metric_name):
@@ -89,7 +48,10 @@ def run_one_pandas_test(metric_name):
                f" Yours value: {metric_result}. Valid value: {expected_result}"
                )
         for key, value in metric_result.items():
-            assert np.isclose(value, expected_result[key], rtol=1e-04), msg
+            if isinstance(expected_result[key], numbers.Number):
+                assert np.isclose(value, expected_result[key], rtol=1e-04), msg
+            else:
+                assert value == expected_result[key], msg
 
 
 def run_one_pyspark_test(metric_name):
@@ -121,7 +83,10 @@ def run_one_pyspark_test(metric_name):
                f" Yours value: {metric_result}. Valid value: {expected_result}"
                )
         for key, value in metric_result.items():
-            assert np.isclose(value, expected_result[key], rtol=1e-04), msg
+            if isinstance(expected_result[key], numbers.Number):
+                assert np.isclose(value, expected_result[key], rtol=1e-04), msg
+            else:
+                assert value == expected_result[key], msg
 
 
 def test_count_total():
@@ -137,6 +102,15 @@ def test_count_zeros():
 def test_count_null():
     run_one_pandas_test("CountNull")
     run_one_pyspark_test("CountNull")
+
+
+def test_count_null_wrong_agg():
+    try:
+        metrics.CountNull(['qty'], 'ALL')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Not acceptable aggregation value is not handled")
 
 
 def test_count_duplicates():
@@ -172,5 +146,119 @@ def test_count_ration_below():
 def test_count_cb():
     run_one_pandas_test("CountCB")
     run_one_pyspark_test("CountCB")
+
+
+def test_count_lag():
+    run_one_pandas_test("CountLag")
+    run_one_pyspark_test("CountLag")
+
+
+def test_count_value_in_required_set():
+    run_one_pandas_test("CountValueRequiredSet")
+    run_one_pyspark_test("CountValueRequiredSet")
+
+
+def test_count_value_in_bounds():
+    run_one_pandas_test("CountValueInBounds")
+    run_one_pyspark_test("CountValueInBounds")
+
+
+def test_count_value_in_bound_wrong_bounds():
+    try:
+        metrics.CountValueInBounds('qty', 10, 5, False)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Lower bound greater than upper bound case is not handled")
+
+
+def test_count_extreme_values_formula():
+    run_one_pandas_test("CountExtremeValuesFormula")
+    run_one_pyspark_test("CountExtremeValuesFormula")
+
+
+def test_count_extreme_values_formula_wrong_style():
+    try:
+        metrics.CountExtremeValuesFormula('qty', 1, 'equal')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Not acceptable style value is not handled.")
+
+
+def test_count_extreme_values_quantile():
+    run_one_pandas_test("CountExtremeValuesQuantile")
+    run_one_pyspark_test("CountExtremeValuesQuantile")
+
+
+def test_count_extreme_values_quantile_wrong_style():
+    try:
+        metrics.CountExtremeValuesQuantile('qty', 0.2, 'equal')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Not acceptable style value is not handled.")
+
+
+def test_count_extreme_values_quantile_wrong_q():
+    try:
+        metrics.CountExtremeValuesQuantile('qty', 2, 'greater')
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Quantile value out of [0, 1] interval is not handled.")
+
+
+def test_count_last_day_rows():
+    run_one_pandas_test("CountLastDayRows")
+    run_one_pyspark_test("CountLastDayRows")
+
+
+def test_count_last_day_rows_negative_percent():
+    try:
+        metrics.CountLastDayRows('day', -10)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Negative percent is not handled.")
+
+
+def test_count_few_last_day_rows():
+    run_one_pandas_test("CountFewLastDayRows")
+    try:
+        run_one_pyspark_test("CountFewLastDayRows")
+    except NotImplementedError:
+        pass
+    else:
+        raise AssertionError("Calling not implemented method is not handled.")
+
+
+def test_count_few_last_day_rows_negative_percent():
+    try:
+        metrics.CountFewLastDayRows('day', -10)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Negative percent is not handled.")
+
+
+def test_count_few_last_day_rows_negative_number():
+    try:
+        metrics.CountFewLastDayRows('day', 40, -2)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Negative number is not handled.")
+
+
+def test_count_few_last_day_rows_number_greater():
+    try:
+        model = metrics.CountFewLastDayRows('day', 40, 8)
+        df = TABLES2["sales"]
+        model(df)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Number greater than number of days in dataset is not handled.")
 
 
